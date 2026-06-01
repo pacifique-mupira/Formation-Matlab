80631f86c040bdc4a5d95a8e567925583ed6e79e
pacifique-mupira <pacifiquemupiraperse@gmail.com>
2026-06-01 22:12:32 +0300

    Suite des modifications

--- test.m
+++ test.m
@@ -1,4 +1,9 @@
+disp("Bienvenue dans notre programme");
 a = 8 + 4;
+
 b = 34;
 c = a + b;
+e = c/b;
 disp(['La reponse vaut ', num2str(c)])
+disp("Merci pour votre fidelité");
+disp(["la reponse vaut : ", num2str(e)]);
